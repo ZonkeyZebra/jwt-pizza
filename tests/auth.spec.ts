ae6134659e86b5ab38b64ed2acc447def0f898f5
ZonkeyZebra <blackcec@yahoo.com>
2026-02-03 16:29:23 -0700

    get to login and register tests, 40% coverage

--- tests/auth.spec.ts
+++ tests/auth.spec.ts
@@ -5,3 +5,46 @@ test('home page', async ({ page }) => {
 
   expect(await page.title()).toBe('JWT Pizza');
 });
+
+test('get to login', async ({ page }) => {
+  await page.goto('http://localhost:5173/');
+  await page.getByRole('link', { name: 'Login' }).click();
+  await expect(page.getByText('Welcome back')).toBeVisible();
+  await expect(page.getByRole('heading')).toContainText('Welcome back');
+  await page.getByRole('textbox', { name: 'Email address' }).click();
+  await expect(page.getByRole('main')).toMatchAriaSnapshot(`
+    - text: Email address
+    - textbox "Email address"
+    - img
+    - text: Password
+    - textbox "Password"
+    - button:
+      - img
+    - img
+    - button "Login"
+    - text: Are you new? Register instead.
+    `);
+});
+
+test('register', async ({ page }) => {
+  await page.goto('http://localhost:5173/');
+  await page.getByRole('link', { name: 'Register' }).click();
+  await page.getByRole('textbox', { name: 'Full name' }).click();
+  await page.getByRole('textbox', { name: 'Full name' }).fill('newTest');
+  await page.getByRole('textbox', { name: 'Email address' }).click();
+  await page.getByRole('textbox', { name: 'Email address' }).fill('new@test.com');
+  await page.getByRole('textbox', { name: 'Email address' }).press('Tab');
+  await page.getByRole('textbox', { name: 'Password' }).fill('testing');
+  await page.getByRole('button', { name: 'Register' }).click();
+  await page.getByRole('link', { name: 'Logout' }).click();
+  await expect(page.locator('#navbar-dark')).toMatchAriaSnapshot(`
+    - link "Order":
+      - /url: /menu
+    - link "Franchise":
+      - /url: /franchise-dashboard
+    - link "Login":
+      - /url: /login
+    - link "Register":
+      - /url: /register
+    `);
+});
